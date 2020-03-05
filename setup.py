85f18d9a2082fa6b626dfc25e69745f203dc6025
Janne Enberg <janne.enberg@lietu.net>
2020-03-05 17:19:37 +0200

    Updated version info etc.

--- setup.py
+++ setup.py
@@ -15,13 +15,14 @@ except(IOError, ImportError, RuntimeError):
 setup(
     name='pypusu',
     packages=['pypusu'],  # this must be the same as the name above
-    version='1.0.5',
+    version='1.0.6',
     description='Python client for PuSuEngine',
     long_description=long_description,
     author='Janne Enberg',
     author_email='janne.enberg@lietu.net',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*,<4",
     url='https://github.com/PuSuEngine/pypusu',
-    download_url='https://github.com/PuSuEngine/pypusu/tarball/v1.0.5',
+    download_url='https://github.com/PuSuEngine/pypusu/tarball/v1.0.6',
     keywords=['pubsub', 'publisher', 'subscriber', 'messaging'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -31,5 +32,10 @@ setup(
         "Topic :: Communications :: Chat",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
